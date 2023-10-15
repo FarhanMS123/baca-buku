fd268dc4138af69e3deed97a345495d053edd99c
Farhan <animationfar@gmail.com>
2023-10-16 00:55:54 +0700

    feat: use mock page to establish page size

--- src/pages/buku/[id].tsx
+++ src/pages/buku/[id].tsx
@@ -2,12 +2,13 @@ import 'react-pdf/dist/Page/AnnotationLayer.css';
 import 'react-pdf/dist/Page/TextLayer.css';
 
 import { useRouter } from "next/router";
-import { RefObject, useEffect, useRef, useState } from "react";
+import { RefObject, useCallback, useEffect, useRef, useState } from "react";
 import { api } from "~/utils/api";
 import { Document, Page } from "react-pdf";
 import { ArrowLeft, Ear, Home, Volume2, VolumeX } from 'lucide-react';
 import Link from 'next/link';
 import { FixedSizeList } from "react-window";
+import { useClientSide } from '~/hooks/solver';
 
 type RectDOMBase = "x" | "y" | "width" | "height";
 type RectDOMPos = "top" | "bottom" | "left" | "right";
@@ -19,88 +20,62 @@ export default function Buku () {
   } = router.query;
   const { data: buku } = api.buku.getBuku.useQuery(parseInt(id ?? "0"), { enabled: Boolean(id) });
 
-  const [numPages, setNumPages] = useState<number>(0);
-  const [pageNumber, setPageNumber] = useState<number>(1);
+  const [isClient] = useClientSide();
+  const [_refreshState, _refresh] = useState<number>();
+  const refreshState = () => void _refresh(Math.random());
 
-  const [d_height, setDHeight] = useState<number>();
-  const [d_bound, setDBound] = useState<Partial<Record<RectDOMBase | RectDOMPos, number>>>({});
+  const [totalPages, setTotalPages] = useState(0);
+  const [visibleStartIndex, setVisibleStartIndex] = useState(0);
+  const [canvasBound, setCanvasBound] = useState<DOMRect>({ x: 0, y: 0, width: 10, height: 10, top: 0, left: 0, right: 10, bottom: 10, toJSON: () => ({}) });
+  const [d_width, setDWidth] = useState<[number, boolean]>([100, false]); // width, required
+  const [d_height, setDHeight] = useState(100);
+
   const [twoPages, setTwoPages] = useState(false);
 
-  const [, setStateAudio] = useState<number>(0);
-
   const refAudio = useRef<HTMLAudioElement>(null);
-  const refCanvas = useRef<HTMLCanvasElement>(null);
-
-  console.log([refCanvas, d_height, d_bound]);
+  const refPageCanvas = useRef<HTMLCanvasElement>(null);
 
   useEffect(() => {
+    // refAudio.current?.load();
+
     const handle = () => {
       if (refAudio.current?.readyState == 0) refAudio.current?.load();
       else if (!refAudio.current?.paused) document.removeEventListener("click", handle);
       
       refAudio.current?.play();
-      setStateAudio(Math.random())
+      refreshState();
     };
+    // document.addEventListener("click", handle);
+    // return () => document.removeEventListener("click", handle);
 
-    document.addEventListener("click", handle);
-
-    refAudio.current?.load();
-
-    return () => document.removeEventListener("click", handle);
-
-  }, [refAudio, buku]);
+  }, [buku]);
 
   useEffect(() => {
-    const handleResize = () => {
-      setDHeight(window.innerHeight);
-    };
     handleResize();
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
+    document.addEventListener("click", handleResize);
+    window.addEventListener('resize', handleResize);
+    return () => {
+      document.removeEventListener("click", handleResize);
+      window.removeEventListener("resize", handleResize);
+    }
   }, []);
 
-  useEffect(() => {
-    handleTwoPage();
-    document.addEventListener("click", handleTwoPage);
-    return () => document.removeEventListener("click", handleTwoPage)
-  }, [refCanvas, d_height, d_bound]);
+  function handleResize() {
+    if (refPageCanvas.current) {
+      const bound = refPageCanvas.current.getBoundingClientRect();
+      setCanvasBound(bound);
 
-  function handleTwoPage() {
-    if (refCanvas.current) {
-      let isTwoPages = false;
-      if (refCanvas.current.width * 2 < window.innerWidth) isTwoPages = true;
-      setTwoPages(isTwoPages);
+      if (bound.width * 2 <= window.innerWidth) setTwoPages(true);
+      else setTwoPages(false);
 
-      const bound = refCanvas.current.getBoundingClientRect();
-      if (refCanvas.current.width <= window.innerWidth) { // Canvas is Fit, use height
-        if (Math.abs(refCanvas.current.height - window.innerHeight) > 50) setDBound(bound);
-      } else { // Canvas is cramped, use width
-        if (Math.abs(refCanvas.current.height - window.innerHeight) > 50) setDBound(bound);
-      }
+      if (window.innerWidth < bound.width) setDWidth([window.innerWidth, true]);
+      else if (window.innerHeight < bound.height) setDWidth([window.innerWidth, false]);
+      else if (window.innerWidth > bound.width) setDWidth([window.innerWidth, false]);
 
-      console.log([refCanvas.current.height, window.innerHeight, Math.abs(refCanvas.current.height - window.innerHeight) > 50]);
+      console.log([window.innerHeight, bound.height, window.innerHeight < bound.height]);
     }
-  }
 
-  function setPage(cur: 1 | -1) {
-    if (cur == 1 && pageNumber < numPages) setPageNumber((x) => x + 1);
-    else if (cur == -1 && pageNumber > 1) setPageNumber((x) => x - 1);
-  }
-
-  function handlePaginationFirst(ev: MouseEvent) {
-    if (twoPages) setPage(-1);
-    else {
-      const bound = refCanvas.current!.getBoundingClientRect();
-      const w_2 = bound.width / 4; 
-      const [x, y] = [ev.clientX - bound.x, ev.clientY - bound.y];
-
-      if (x < w_2) setPage(-1);
-      else if (x > w_2 * 3) setPage(1);
-    }
-  }
-
-  function onDocumentLoadSuccess({ numPages }: { numPages: number }): void {
-    setNumPages(numPages);
+    setDHeight(window.innerHeight);
   }
 
   if (!buku) return (<></>);
@@ -118,24 +93,29 @@ export default function Buku () {
         <Ear />
       </button>
       <button className="btn btn-circle bg-base-content text-base-100 shadow-lg border-0"
-        onClick={() => {refAudio.current?.paused ? refAudio.current?.play() : refAudio.current?.pause(); setStateAudio(Math.random());} }
+        onClick={() => {refAudio.current?.paused ? refAudio.current?.play() : refAudio.current?.pause(); refreshState();} }
       >
         {refAudio.current?.paused ? <VolumeX /> : <Volume2 />}
       </button>
     </div>
 
-    <Document file={buku.blob_url} onLoadSuccess={onDocumentLoadSuccess} className="flex justify-center items-center h-screen sm:h-auto">
+    <Document file={buku.blob_url} onLoadSuccess={({ numPages }) => void setTotalPages(numPages)} className="relative">
+      <Page canvasRef={refPageCanvas} pageIndex={visibleStartIndex} 
+        height={d_height} width={d_width[1] ? d_width[0] : undefined}
+        className="!bg-transparent absolute -z-10 -top-full -left-full !max-w-min" 
+      />
       <FixedSizeList
-        height={d_height ?? 10}
-        itemSize={d_bound.width ?? 10}
         layout="horizontal"
-        itemCount={numPages}
-        width={(d_bound.width ?? 0) * 2}
-        className="!overflow-hidden"
+        itemCount={totalPages}
+        itemSize={d_width[0]}
+        // width={twoPages ? canvasBound.width * 2 : canvasBound.width}
+        width={canvasBound.width}
+        height={d_height}
+        onItemsRendered={({ visibleStartIndex }) => setVisibleStartIndex(visibleStartIndex)}
       >
-        {({index, style}) => (
-          <div style={{...style}}>
-            <Page {...(index == 0 ? {canvasRef: refCanvas} : {})} pageIndex={index} height={d_height} />
+        {({ index, style }) => (
+          <div style={style}>
+            <Page pageIndex={index} height={d_height} width={d_width[1] ? d_width[0] : undefined} className="!max-w-min" />
           </div>
         )}
       </FixedSizeList>
